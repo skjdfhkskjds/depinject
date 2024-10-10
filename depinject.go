2c8344b33d8d57e426785f03430ec6ec8e2a7e36
skjdfhkskjds <colin.chuu@gmail.com>
2024-10-11 07:08:01 +0800

    container access via funcs without explicit instance

--- depinject.go
+++ depinject.go
@@ -14,3 +14,19 @@ type Container = depinject.Container
 
 // NewContainer returns a new, valid container.
 var NewContainer = depinject.NewContainer
+
+// Global container instance for users who would rather not
+// manage their own container instances.
+var c = NewContainer()
+
+func Invoke(outputs ...any) error {
+	return c.Invoke(outputs...)
+}
+
+func Provide(constructors ...any) error {
+	return c.Provide(constructors...)
+}
+
+func Supply(values ...any) error {
+	return c.Supply(values...)
+}
